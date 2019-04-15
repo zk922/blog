f2d8c575aa07bdf9e94499d81f9327299bda247e
zhangkuo <zhangk922@163.com>
2019-04-15 16:01:15 +0800

    dev

--- src/pages/list/list.component.ts
+++ src/pages/list/list.component.ts
@@ -9,7 +9,9 @@ export class ListComponent implements OnInit {
 
   constructor() { }
   public list: any[] = [
-    {t: 1}, {t: 2}, {t: 3}
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'}
   ];
   ngOnInit() {
   }
