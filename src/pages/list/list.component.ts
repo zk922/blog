b31ba74e3c7b9ab06d98b3d69b719dbe4569c4aa
zhangkuo <zhangk922@163.com>
2019-05-05 18:38:25 +0800

    dev

--- src/pages/list/list.component.ts
+++ src/pages/list/list.component.ts
@@ -2,7 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {fromEvent, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
@@ -19,11 +19,25 @@ export class ListComponent implements OnInit, OnDestroy {
   public list: any[] = [
     {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
     {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
-    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'}
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
+    {title: '去微软推哦哦i破iu哦怕怕怕', tags: [], date: '2019-03-04'},
   ];
 
-  public getArticleList: Observable<any> = fromEvent(window, 'scroll').pipe(map(e => {}));
-  public fetchingData = false;
+  // todo 考虑如何实现滚动无限加载
+  public getArticleList: Observable<any> = fromEvent(window, 'scroll').pipe(
+    filter(() => !this.fetchingData),
+
+  );
+  public fetchingData: boolean = false;
+  public page: number = 0;
 
   ngOnInit() {
   }
