c698a377b8253edf1f975b58aa4e8b37bb99ad5c
zk922 <zhangk922@163.com>
2019-06-04 23:24:17 +0800

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {EnvironmentModule} from '../environments/environment.module';
 import {ListModule} from '../pages/list/list.module';
-import {CommonModule} from '@angular/common';
-import {ArticleModule} from '../pages/article/article.module';
-import {PostModule} from '../pages/post/post.module';
-import {Page404Module} from '../pages/page404/page404.module';
+import {CommonModule, registerLocaleData} from '@angular/common';
+import { ArticleModule } from '../pages/article/article.module';
+import { PostModule } from '../pages/post/post.module';
+import { Page404Module } from '../pages/page404/page404.module';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -21,9 +28,13 @@ import {Page404Module} from '../pages/page404/page404.module';
     ListModule,
     ArticleModule,
     PostModule,
-    Page404Module
+    Page404Module,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/pages/article/article.module.ts
+++ src/pages/article/article.module.ts
@@ -1,6 +1,6 @@
-import {NgModule} from '@angular/core';
-import {ArticleComponent} from './article.component';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ArticleComponent } from './article.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
